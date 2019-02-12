f344b8aad10179e3545d54231208f4617ee8bc64
Endel Dreyer <endel.dreyer@gmail.com>
2019-02-12 21:56:07 -0200

    use 'public' directory on production (heroku)

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = function (options) {
     },
     output: {
       filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist')
+      path: path.resolve(__dirname, 'public')
     }
   };
 }
